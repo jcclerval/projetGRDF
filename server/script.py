5086cbfe83931386a1751bb2495a879593addd6a
Jean-Christophe <jcclerval@aol.com>
2018-03-01 10:34:51 +0100

    ajout de largument temps pour la lecture

--- server/script.py
+++ server/script.py
@@ -100,7 +100,7 @@ def deleteContent(camionId):
         con = mdb.connect(host=host, user=user, passwd=password, db=bdd)
         cur = con.cursor()
         try:
-            cur.execute("DELETE FROM effectifs WHERE idcamion={camionId};".format(camionId=camionId))
+            cur.execute("DELETE FROM effectifs WHERE idcamion={camionId} AND idoutil <> {null};".format(camionId=camionId, null='NULL'))
         except:
             pass
     except mdb.Error, e:
