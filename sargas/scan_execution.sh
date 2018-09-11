f2c662252a53ec2c51ca0ff0087c6ba9ad21402f
jcclerval <jcclerval@aol.com>
2018-09-11 22:30:02 +0200

    cron tables

--- sargas/scan_execution.sh
+++ sargas/scan_execution.sh
@@ -3,7 +3,22 @@
 sleep 3
 echo "Lancement d'un scan"
 #
-# Ici le code
+i="0"
+
+wvdial &
+while [ $i -lt 10 ]
+	do
+	# Test de la connection internet
+	wget -q --tries=10 --timeout=20 --spider http://google.com
+	if [[ $? -eq 0 ]]; then
+			echo "Online"
+			echo "proceed with programm"
+	else
+			echo "Offline"
+			sleep 2
+	fi
+	i=$[$i+1]
+done
 #
 echo "Fin du code"
 sleep 2
