e704e1bb955dab926476b89546d63bf2b2f28194
jcclerval <jcclerval@aol.com>
2018-09-11 22:36:27 +0200

    wvdial programme

--- sargas/scan_execution.sh
+++ sargas/scan_execution.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 sleep 3
-echo "Lancement d'un scan"
+echo "--- Lancement d'un scan ---"
 #
 i="0"
-
-wvdial &
+echo "--- Lancement de Wvdial ---"
+#wvdial &
+sleep 2
+echo "--- Test de connection ---"
 while [ $i -lt 10 ]
 	do
+	echo "Tentative $i"
 	# Test de la connection internet
 	wget -q --tries=10 --timeout=20 --spider http://google.com
 	if [[ $? -eq 0 ]]; then
 			echo "Online"
 			echo "proceed with programm"
+			i="10"
 	else
 			echo "Offline"
 			sleep 2
