ee4405f5a87299897d4fbc35f362f698c40677d9
jcclerval <jcclerval@aol.com>
2018-09-23 20:11:50 +0200

    corrections

--- sargas/scan_execution.sh
+++ sargas/scan_execution.sh
@@ -23,7 +23,7 @@ while [ $i -le $max_count ]
 	if [[ $? -eq 0 ]]; then
 			echo "Online"
 			echo "proceed with programm"
-			python $path0sargas/script.py
+			python $path0'sargas/script.py'
 			killall wvdial
 			break
 	else
