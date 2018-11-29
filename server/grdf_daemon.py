9e56c13daa9e0c4f2e60979f57a8e3044cda0a12
Jean-Christophe <jcclerval@aol.com>
2018-11-29 22:23:23 +0100

    ajouts date

--- server/grdf_daemon.py
+++ server/grdf_daemon.py
@@ -136,7 +136,7 @@ def updateData(camion, data):
 			cur.execute("INSERT INTO effectifs VALUES(NULL, '{camion}', '{outil}', '{idetiquette}');".format(camion=camion, outil = data[:suffix], idetiquette=data))
 			cur.execute("UPDATE camion SET derniereCo='{}' WHERE plaque='{}'".format('{}-{}-{}'.format(nw.year, nw.month, nw.day),camion))
 			with open('/home/grdf/logs/log', 'a') as log:
-				print >> log, '{}-{}-{}  --- {} --- {}'.format(nw.year, nw.month, nw.day, camion, data[:suffix])
+				print >> log, '{}-{}-{}  --- {} --- {}'.format(nw.year, nw.month, nw.day, camion, data)
 		except:
 			print 'error'
 			pass
