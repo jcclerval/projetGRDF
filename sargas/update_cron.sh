a564fc3bad1fe3d5b3895446da08f7eb637a151b
root <root@Sargas-844b6f.localdomain>
2019-06-28 17:44:16 +0200

    ajouts crontab

--- sargas/update_cron.sh
+++ sargas/update_cron.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 crontab -r
-line='*/30 9-17 * * mon,tue,wed,thu,fri /home/debian/ProjetGRDF/sargas/scan_execution.sh >> /home/debian/ProjetGRDF/sargas/sauvegarde.log'
-(crontab -u root -l; echo "$line" ) | crontab -u root -
+##line='*/30 9-17 * * mon,tue,wed,thu,fri /home/debian/ProjetGRDF/sargas/scan_execution.sh >> /home/debian/ProjetGRDF/sargas/sauvegarde.log'
+line_1='0 9-17/2 * * mon,tue,wed,thu,fri /home/debian/ProjetGRDF/sargas/scan_execution.sh >> /home/debian/ProjetGRDF/sargas/sauvegarde.log='
+line_2='0 9-17/2 * * mon,tue,wed,thu,fri time timeout 3600 ssh grdf >> /home/debian'
+
+
+(crontab -u debian -l; printf "$line_1 \n$line_2\n" ) | crontab -u debian -
