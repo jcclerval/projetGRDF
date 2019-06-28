6be677cdf5ef41713721e2bea5240c9b941d5876
root <root@Sargas-844b6f.localdomain>
2019-06-28 18:36:27 +0200

    corrections cron

--- sargas/update_cron.sh
+++ sargas/update_cron.sh
@@ -3,7 +3,8 @@
 crontab -r
 ##line='*/30 9-17 * * mon,tue,wed,thu,fri /home/debian/ProjetGRDF/sargas/scan_execution.sh >> /home/debian/ProjetGRDF/sargas/sauvegarde.log'
 line_1='0 9-17/2 * * mon,tue,wed,thu,fri /home/debian/ProjetGRDF/sargas/scan_execution.sh >> /home/debian/ProjetGRDF/sargas/sauvegarde.log='
-line_2='0 9-17/2 * * mon,tue,wed,thu,fri time timeout 3600 ssh grdf >> /home/debian'
+line_2='0 9-17/2 * * mon,tue,wed,thu,fri timeout 3600 autossh grdf -N >> /home/debian'
+line_3='@reboot timeout 3600 autossh grdf -N >> /home/debian'
 
 
-(crontab -u debian -l; printf "$line_1 \n$line_2\n" ) | crontab -u debian -
+(crontab -u debian -l; printf "$line_1 \n$line_2\n$line_3\n" ) | crontab -u debian -
